430a2fdb88d6266894c09b3a62164586ee7b5622
shigaki daichi <d-shigaki@pro-seeds.co.jp>
2019-03-12 18:40:13 +0900

    wip

---
+++ src/components/Form.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="form">
+
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <router-link to="/">ユーザーの一覧</router-link>
+      </ol>
+    </nav>
+
+    <h1>ユーザーの新規登録</h1>
+
+  <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">名前</label>
+    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">性別</label>
+    <input type="password" class="form-control">
+  </div>
+  <button type="submit" class="btn btn-primary">登録</button>
+</form>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Form'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>ユーザーの一覧</h1>
+
+    <ul class="nav">
+      <li class="nav-item">
+        <router-link to="/create">新規登録</router-link>
+      </li>
+    </ul>
+
+    <ul class="list-group">
+      <li class="list-group-item">さる</li>
+      <li class="list-group-item">きつね</li>
+      <li class="list-group-item">たぬき</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+</style>
